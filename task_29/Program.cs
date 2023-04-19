76257082bfef37cf3fc133746a461160a1940c7a
Voinsckih <129402608+Voinsckih@users.noreply.github.com>
2023-04-19 16:22:33 +0500

    Add files via upload

---
+++ task_29/Program.cs
@@ -0,0 +1,13 @@
+﻿int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+ randomArray[i] = new Random().Next(1,99);
+ Console.Write(randomArray[i] + ",");
+}
+int ReadInt(string message)
+{
+ Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine());
+}
